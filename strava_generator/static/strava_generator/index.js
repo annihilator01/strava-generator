4ef08a9060f84d365ad29e43a87096c5e1443b91
Ilia Dubrovskii <ghrom07@yandex.ru>
2021-04-23 03:19:48 +0300

    Add basic files for frontend and connection to google maps js

---
+++ strava_generator/static/strava_generator/index.js
@@ -0,0 +1,8 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map($('#map')[0], {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
--- strava_generator/views.py
+++ strava_generator/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from . import service
 
 
 def index(request):
+    service.register_visitor(request)
     return render(request, 'strava_generator/index.html')
