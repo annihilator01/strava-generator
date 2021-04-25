8870ce7d77bfb0ee4c741c0e0159041d41985b0d
Ilia Dubrovskii <iliadubrovskii01@gmail.com>
2021-04-25 14:04:45 +0300

    Create draft of embedding map

--- strava/settings.py
+++ strava/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['CONTEXT'] == 'DEBUG' else False
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'strava_generator.apps.StravaGeneratorConfig',
     'rest_framework',
+    "sslserver",
 ]
 
 MIDDLEWARE = [
--- strava_generator/static/strava_generator/index.js
+++ strava_generator/static/strava_generator/index.js
@@ -1,8 +1,24 @@
 let map;
 
-function initMap() {
-  map = new google.maps.Map($('#map')[0], {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+
+function getCurrentLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
+}
+
+
+async function initMap() {
+  let center_coords = new google.maps.LngLat(55.755799012637766, 37.617742567387175);
+
+  getCurrentLocation()
+      .then(position => {
+        center_coords = new google.maps.LngLat(position.coords.latitude, position.coords.longitude);
+      })
+      .finally(onFinally => {
+        map = new google.maps.Map($('#map')[0], {
+          center: LatLng,
+          zoom: 17
+        });
+      })
 }
