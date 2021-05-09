5b85e9aceda70e93ad5b14150dbd2af29cad747d
Ilia Dubrovskii <ghrom07@yandex.ru>
2021-05-09 22:19:06 +0300

    Divide db into debug and production

--- strava/settings.py
+++ strava/settings.py
@@ -84,12 +84,20 @@ WSGI_APPLICATION = 'strava.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3' if not DEBUG else BASE_DIR / 'db_debug.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'debug.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'production.sqlite3'),
+        }
+    }
 
 
 # Password validation
